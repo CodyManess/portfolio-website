edfbaa5e0ca61cd13dfb6da56c86e48ec40decde
Cody Maness <cody.maness@outlook.com>
2025-01-12 16:01:21 -0600

    feat: moving from mui accordion

--- app/components/Experience/RoleCard/roleCard.tsx
+++ app/components/Experience/RoleCard/roleCard.tsx
@@ -1,13 +1,12 @@
+import * as React from 'react'
+import styles from './roleCard.module.css'
 import Role from '@/app/models/role'
 import {
   Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Typography,
-} from '@mui/material'
-import * as React from 'react'
-import { FaAngleDown } from 'react-icons/fa'
-import styles from './roleCard.module.css'
+  AccordionBody,
+  AccordionHeader,
+} from '../../Shared/Accordion/accordion'
+import { Typography } from '@mui/material'
 
 interface RoleCardProps {
   role: Role
@@ -18,34 +17,24 @@ interface RoleCardProps {
 
 const RoleCard = ({ role, setExpanded, expanded, index }: RoleCardProps) => {
   return (
-    <Accordion
-      expanded={expanded === index}
-      onChange={() => setExpanded(index === expanded ? false : index)}
-    >
-      <AccordionSummary
-        aria-controls={'panel' + index + '-content'}
-        id={'panel' + index + '-header'}
-        expandIcon={
-          <FaAngleDown size="1.5em" style={{ color: 'var(--foreground)' }} />
-        }
-      >
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <Typography variant="h6" component="h3">
-            {role.title}
-          </Typography>
-          <Typography variant="subtitle1" component="p">
-            {role.companyName}
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            component="p"
-            className={styles.companyDateMobile}
-          >
-            {role.date}
-          </Typography>
-        </div>
-      </AccordionSummary>
-      <AccordionDetails>
+    <Accordion index={index} expanded={expanded} setExpanded={setExpanded}>
+      <AccordionHeader>
+        <Typography variant="h6" component="h3">
+          {role.title}
+        </Typography>
+        <Typography variant="subtitle1" component="p">
+          {role.companyName}
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          component="p"
+          className={styles.companyDateMobile}
+        >
+          {role.date}
+        </Typography>
+      </AccordionHeader>
+
+      <AccordionBody>
         {role.description.map((section, index) => (
           <div key={`${role.title}${index}`}>
             <Typography component="h5">{section.heading}</Typography>
@@ -56,7 +45,7 @@ const RoleCard = ({ role, setExpanded, expanded, index }: RoleCardProps) => {
             </ul>
           </div>
         ))}
-      </AccordionDetails>
+      </AccordionBody>
     </Accordion>
   )
 }
---
+++ app/components/Shared/Accordion/accordion.tsx
@@ -0,0 +1,87 @@
+import React, { useRef } from 'react'
+import styles from './accordion.module.css'
+import { FaAngleDown } from 'react-icons/fa'
+
+interface AccordionProps {
+  index: number
+  expanded: number | false
+  setExpanded: (index: number | false) => void
+  children: React.ReactNode
+}
+
+interface AccordionChildrenProps {
+  children: React.ReactNode
+}
+
+export function Accordion(props: AccordionProps) {
+  const accordionBody = useRef<HTMLDivElement>(null)
+  const accordionHeader = useRef<HTMLButtonElement>(null)
+
+  let header
+  let body
+
+  const toggleAccordion = () => {
+    if (props.expanded === props.index) {
+      accordionHeader.current?.blur()
+    }
+    props.setExpanded(props.expanded === props.index ? false : props.index)
+  }
+
+  React.Children.forEach(props.children, (child) => {
+    if (!React.isValidElement(child)) return
+    if (child.type === AccordionHeader) {
+      header = child
+    } else if (child.type === AccordionBody) {
+      body = child
+    }
+  })
+
+  return (
+    <div
+      className={`${styles.accordionCard} ${props.expanded === props.index ? styles.active : ''}`}
+    >
+      {!!header && (
+        <button
+          ref={accordionHeader}
+          className={styles.accordionHeader}
+          onClick={toggleAccordion}
+          aria-expanded={props.expanded === props.index}
+          aria-controls={`accordionpanel${props.index}`}
+          id={`accordion${props.index}`}
+        >
+          <div className={styles.accordionTitle}>{header}</div>
+          <FaAngleDown className={styles.accordionIcon} />
+        </button>
+      )}
+
+      {!!body && (
+        <div
+          ref={accordionBody}
+          className={`${styles.accordionBody}`}
+          id={`accordionpanel${props.index}`}
+          role="region"
+          aria-labelledby={`accordion${props.index}`}
+          style={
+            props.expanded === props.index
+              ? {
+                  height: accordionBody.current
+                    ? accordionBody.current.scrollHeight
+                    : 'auto',
+                }
+              : { height: '0px' }
+          }
+        >
+          <div className={styles.accordionBodyContent}>{body}</div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export function AccordionHeader(props: AccordionChildrenProps) {
+  return <>{props.children}</>
+}
+
+export function AccordionBody(props: AccordionChildrenProps) {
+  return <>{props.children}</>
+}
